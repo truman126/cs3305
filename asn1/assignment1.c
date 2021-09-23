1fbfb6efba50977b42a9bdbdefb99a4ff92a0547
truman126 <trumanjohnston@gmail.com>
2021-09-22 21:42:42 -0400

    all done

--- asn1/assignment1.c
+++ asn1/assignment1.c
@@ -9,13 +9,11 @@
 #define PATH "external_program.out"
 
 //create parent
-int main()
+int main(int argc, char **argv)
 {
 
 	//create child_1
 	pid_t pid = fork();
-
-	// printf("pid is %d", pid);
 
 	if (pid < 0) // fork unsuccessful
 	{
@@ -25,15 +23,19 @@ int main()
 	if (pid > 0) //parent
 	{
 
+
+
 		//wait for child_1 to terminate
 		wait(NULL);
 
 		//create child_2
+
 		pid = fork();
 
 		//check if child 2 is successful
 		if (pid < 0)
 		{
+
 			printf("The fork as unsuccessful (2/3) \n");
 
 			// exit(1);
@@ -42,9 +44,12 @@ int main()
 		// catches child_2
 		else if (pid == 0)
 		{
+			printf("parent(PID %d) created child_2(PID %d)\n", getppid(), getpid());
+
 			// create child 2.1
 
 			pid = fork();
+			wait(NULL);
 
 			if (pid > 0)
 			{
@@ -53,6 +58,23 @@ int main()
 			//catches child_2.1
 			else if (pid == 0)
 			{
+				printf("child_2(PID %d) created child_2.1(PID %d)\n", getppid(), getpid());
+
+				//
+
+				char child_name[] = " for child_2.1";
+				//convert pid to a string
+				pid_t current_pid = getpid();
+				// ex. 34567 for child_2.1
+				char arg[21];
+
+				sprintf(arg, "%d", current_pid);
+
+				strcat(arg, child_name);
+
+				printf("child_2.1 (PID %d) is calling an external program external_program.out and leaving parent\n", getpid());
+
+				execl(argv[1], PATH, arg, NULL);
 
 				// call_external_program(" for child_2.1");
 
@@ -60,15 +82,16 @@ int main()
 			}
 			else if (pid > 0)
 			{
+				printf("child_2 (PID %d) is waiting for child_2.1 (PID %d) to complete before creating child_2\n", getppid(), getpid());
 
-				wait(NULL);
-
-				exit(1);
 			}
+			exit(1);
 		}
 
 		//wait for child_2 to terminate
 		wait(NULL);
+
+		printf("child_1 and child_2 are completed and parent process is terminating…\n");
 
 		//the parent process is ready to terminate
 		// exit(1);
@@ -77,7 +100,12 @@ int main()
 	//catches child 1
 	if (pid == 0)
 	{
-		printf("first child");
+
+
+		printf("parent(PID %d) created child_1(PID %d)\n", getppid(), getpid());
+		printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n", getppid(), getpid());
+
+
 		char child_name[] = " for child_1";
 		//convert pid to a string
 		pid_t current_pid = getpid();
@@ -88,19 +116,19 @@ int main()
 
 		strcat(arg, child_name);
 
-		printf("\nstring:%s\n",arg);
+		printf("child_1 (PID %d) is calling an external program external_program.out and leaving parent\n", getpid());
 
+				// execl(*argv, PATH, arg, (char*) NULL);
 
-		execl(PATH, arg, arg );
-
-		
+		exit(1);
 
 		//call external_program.out /
 		// exit(1);
-
 	}
 
 	// printf("process");
+
+
 
 	return 0;
 }
--- asn1/external_program.c
+++ asn1/external_program.c
@@ -3,9 +3,9 @@
 int main(int argc, char **argv)
 {
 
-    for (int i =0; i < argc; i++){
-        printf("argv[%d]=%s\n",i , argv[i]);
-    }
+    // for (int i =0; i < argc; i++){
+    //     printf("argv[%d]=%s\n",i , argv[i]);
+    // }
 
     printf("From the external program: The PID was %s\n", argv[1]);
 }
