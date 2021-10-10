9a0817a097c110c6293929504a469e45a31db2b6
truman126 <trumanjohnston@gmail.com>
2021-10-10 01:10:32 -0400

    asn3 done

--- asn3/assignment3.c
+++ asn3/assignment3.c
@@ -5,51 +5,62 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void *sum(int port[])
+//global variable for the pipe
+int port[2];
+
+void *sum(void *thread_id)
 {
     //initialize variables to store X and Y
-    int X, Y, S;
+    int X = 1, Y = 2, S;
 
     //read X and Y from the pipe
-    read(port[0], &X, sizeof(int));
-    read(port[0], &Y, sizeof(int));
+    read(port[0], (&X, &Y), sizeof(int) * 2);
 
-    printf("Thread (%d) reads X = %d and Y = %d from the pipe\n", 100, X, Y);
+    printf("Thread (%d) reads X = %d and Y = %d from the pipe\n", thread_id, X, Y);
 
     //write X + Y to the pipe
     S = X + Y;
     write(port[1], &S, sizeof(int)); // writing twice
-    printf("Thread (%d) writes X + Y = %d to the pipe\n", 100, S);
+    printf("Thread (%d) writes X + Y = %d to the pipe\n", thread_id, S);
 
     return 0;
 }
 
-void *odd_even(int port[])
+void *odd_even(void *thread_id)
 {
 
     int S;
 
     //read S from the pipe
     read(port[0], &S, sizeof(int));
+    printf("Thread (%d) reads X + Y = %d from the pipe\n", thread_id, S);
 
+
+
+    //determine if odd or even
     if (S % 2 == 0)
     {
-        printf("Thread (%d) identifies X + Y = %d as an even number\n", 101, S);
+        printf("Thread (%d) identifies X + Y = %d as an even number\n", thread_id, S);
     }
     else
     {
-        printf("Thread (%d) identifies X + Y = %d as an odd number\n", 101, S);
+        printf("Thread (%d) identifies X + Y = %d as an odd number\n", thread_id, S);
     }
+    
+    printf("Thread (%d) writes X + Y = %d to the pipe\n", thread_id, S);
     write(port[1], &S, sizeof(int));
+    
     return 0;
 }
 
-void *digit_count(int port[])
+void *digit_count(void *thread_id)
 {
     int S, digit_count = 0, S_copy;
 
     //read S from the pipe
     read(port[0], &S, sizeof(int));
+    printf("Thread (%d) reads X + Y = %d from the pipe\n", thread_id, S);
+
 
     S_copy = S;
     do
@@ -60,11 +71,12 @@ void *digit_count(int port[])
 
     } while (S_copy != 0);
 
-    printf("Thread (%d) identifies X + Y = %d as a %d digit number\n", 102, S,  digit_count);
+    printf("Thread (%d) identifies X + Y = %d as a %d digit number\n", thread_id, S, digit_count);
 
     return 0;
 }
 
+int port[2];
 
 int main(int argc, char **argv)
 {
@@ -75,29 +87,36 @@ int main(int argc, char **argv)
 
     printf("Parent (%d) receives X = %d and Y = %d from the user\n", getpid(), X, Y);
 
+
     //create the pipe
     int status;
-    int port[2];
-
     status = pipe(port);
 
+
     //write to the port
+    write(port[1], (&X, &Y), sizeof(int) * 2);
+
     printf("Parent (%d) writes X = %d and Y = %d to the pipe\n", getpid(), X, Y);
-    write(port[1], &X, sizeof(int));
-    write(port[1], &Y, sizeof(int));
 
     //initialize and create the threads
-    pthread_t thread_1; //thread 100
-    pthread_t thread_2; //thread 101
-    pthread_t thread_3; //thread 102
+    pthread_t thread_1, thread_2, thread_3;
 
-    pthread_create(&thread_1, NULL, sum, port); //thread 100
+    //define thread ids
+    int thread_id_1 = 100, thread_id_2 = 101, thread_id_3 = 102;
+
+
+
+    //create and run each thread
+
+    pthread_create(&thread_1, NULL, sum, thread_id_1); //thread 100
     pthread_join(thread_1, NULL);
 
-    pthread_create(&thread_2, NULL, odd_even, port); //thread 101
+
+    pthread_create(&thread_2, NULL, odd_even, thread_id_2); //thread 101
     pthread_join(thread_2, NULL);
 
-    pthread_create(&thread_3, NULL, digit_count, port); //thread 102
+
+    pthread_create(&thread_3, NULL, digit_count, thread_id_3); //thread 102
     pthread_join(thread_3, NULL);
 
     return 1;
